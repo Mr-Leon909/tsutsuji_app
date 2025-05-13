45741b955a44bfcb0c3aa17b315cfd70fbdef385
Mr-Leon909 <dsds45@icloud.com>
2025-05-13 23:41:03 +0900

    Updated Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -15,7 +15,7 @@ export default function Logo({ variant = 'default' }: LogoProps) {
   // 小さいサイズのロゴ（ヘッダー用など）
   return (
     <div className="flex items-center">
-      <img src="../TSUTSUjiロゴ.png" alt="TSUTSUJI" className="w-50 h-50" />
+      <img src="../TSUTSUjiロゴ（改）.png" alt="TSUTSUJI" className="w-50 h-50" />
     </div>
   );
 }
